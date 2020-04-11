524d485908466bd99352522bfa93584bb20a573f
Viktorya SVA <viktorya.scherba@gmail.com>
2020-04-11 22:18:11 +0300

    4 commit

--- java.js
+++ java.js
@@ -1,10 +1,13 @@
-let inputIn = document.querySelector('.input-in');
-let button = document.querySelector('button');
+// document.querySelector('button').onclick = () => {
+//     console.log(document.querySelector('#one').value);
+//     document.querySelector('button').style.backgroundColor = document.querySelector('#one').value;
+// }
 
-button.onclick = function() {
-    // console.log('work');
-    let b = +inputIn.value;
-    console.log( b + 10 );
-    inputIn.value = '';
+// document.querySelector('#one').oninput = () => {
+//     console.log(document.querySelector('#one').value);
+//     document.querySelector('span').innerHTML = document.querySelector('#one').value
+// }
 
-};
+document.querySelector('#btn-1').onclick = () => {
+    console.log(document.querySelector('#i2').value);
+}
