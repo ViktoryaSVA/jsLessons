05714bb18c2573bc9d554379cf497faadf25efc7
Viktorya SVA <viktorya.scherba@gmail.com>
2020-04-11 20:30:14 +0300

    second commit

---
+++ java.js
@@ -0,0 +1,9 @@
+let inputIn = document.querySelector('.input-in');
+let button = document.querySelector('button');
+
+button.onclick = function() {
+    // console.log('work');
+    let b = +inputIn.value;
+    console.log( b + 10 );
+
+};
