addad88830f266bb39750960ab5e0fc9d6291d64
Viktorya SVA <viktorya.scherba@gmail.com>
2020-04-12 23:06:21 +0300

    commit3

--- java.js
+++ java.js
@@ -1,37 +1,10 @@
-// document.querySelector('button').onclick = () => {
-//     console.log(document.querySelector('#one').value);
-//     document.querySelector('button').style.backgroundColor = document.querySelector('#one').value;
-// }
-
-// document.querySelector('#one').oninput = () => {
-//     console.log(document.querySelector('#one').value);
-//     document.querySelector('span').innerHTML = document.querySelector('#one').value
-// }
-
-document.querySelector('#btn-1').onclick = () => {
-    console.log(document.querySelector('#i2').value);
-    let myCheckbox = document.querySelector('#i2');
-    console.log(myCheckbox.checked);
-
-    if (myCheckbox.checked){
-        console.log('on');
-
-    } else {
-        console.log('off');
-
+document.querySelector('button').onclick = () => {
+    let r = document.querySelectorAll('input[type = "radio"]');
+    console.log(r);
+    for (let i = 0; i < r.length; i++){
+        if (r[i].checked){
+            console.log(r[i].value);
+        }
+        
     }
-}
-
-document.querySelector('#btn-2').onclick = (event) => {
-    event.preventDefault();
-    // let text = document.querySelector('#two');
-    // console.log(text.value);
-    // text.value = 'one';
-    let form = document.querySelector('form');
-    console.log(form);
-    console.log(form.elements.two.value);
-    console.log(form.elements.three.value);
-
-
-}
-
+};
