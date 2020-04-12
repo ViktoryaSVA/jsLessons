5bd527349cc405b7b9cac22a1cb28f0c1d36ffdc
Viktorya SVA <viktorya.scherba@gmail.com>
2020-04-12 22:37:00 +0300

    commit

--- java.js
+++ java.js
@@ -10,4 +10,28 @@
 
 document.querySelector('#btn-1').onclick = () => {
     console.log(document.querySelector('#i2').value);
+    let myCheckbox = document.querySelector('#i2');
+    console.log(myCheckbox.checked);
+
+    if (myCheckbox.checked){
+        console.log('on');
+
+    } else {
+        console.log('off');
+
+    }
 }
+
+document.querySelector('#btn-2').onclick = (event) => {
+    event.preventDefault();
+    // let text = document.querySelector('#two');
+    // console.log(text.value);
+    // text.value = 'one';
+    let form = document.querySelector('form');
+    console.log(form);
+    console.log(form.elements.two.value);
+    console.log(form.elements.three.value);
+
+
+}
+
