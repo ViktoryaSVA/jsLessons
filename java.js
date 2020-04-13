898677f79ac4bd5b8d02d95e0aaa2935e12bb6e7
Viktorya SVA <viktorya.scherba@gmail.com>
2020-04-13 22:13:55 +0300

    commit

--- java.js
+++ java.js
@@ -9,5 +9,9 @@ let out = document.querySelector('.out');
 // }
 
 for (let i = 1; i < 10; i++) {
-    out.innerHTML +='3*' + i + '=' + (i * 3) + '<br>';
+
+    for (let k = 1; k < 10; k++) {
+        out.innerHTML +=`${i}* ${k} = ${k * i} <br>`;
+    }
+    out.innerHTML += "<hr>";
 };
